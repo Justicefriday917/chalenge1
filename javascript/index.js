09e202a26f11566c82c56030d1f7131618938a12
Justicefriday917 <justicefriday917@gmail.com>
2022-11-26 15:29:40 +0100

    my first assignment

---
+++ javascript/index.js
@@ -0,0 +1,31 @@
+// let noun = ["Emma","Justice","Edikan","Paul"]
+// let verb = ["kick","loves","likes","prefers"]
+// let adjective = ["Bro","Boss","Mr","Lecturer"]
+// function sentence(x,y,z){
+//     console.log("The man said that "+ noun[x] + " was part of the people who "+verb[z] + " football" )
+//     console.log("During the fight "+ adjective[y],noun[x] + " killed the lion ")
+//     console.log("He "+ verb[z],noun[z] + " to  me ")
+//     console.log(noun[z] ,verb[z] + " eating it raw")
+//     console.log("please can you call me " +adjective[y],noun[z])
+// }
+// sentence(1,3,3)
+
+// almighty formula
+// function almightyFomula(a,b,c){
+//     d = Math.sqrt((b**2)-4*a*c);
+//     if(d < 0){
+//         console.log("undefined")
+//     }
+//     else{
+//         console.log(x1 = (-b - d)/2*a);
+//         console.log(x1 = (-b + d)/2*a);
+
+//     }
+// };
+// almightyFomula(2,3,1);
+
+
+// let NamesOfStudents = ["paul", "Chioma", "Precious","Thessy","blessing","brenda","chisom","Justice","Edikan"]
+// console.log(NamesOfStudents[7])
+
+
